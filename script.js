fd435c7394cebc1790a7abe870954316fc426373
Myriam <myriamb2010@yahoo.fr>
2019-05-07 16:24:51 +0200

    ajout section metier

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.tooltipped');
+   var instances = M.Tooltip.init(elems, 0, 'top');
+ });
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, 0, 'top');
+  });
