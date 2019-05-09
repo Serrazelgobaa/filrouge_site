456d21479caf93eee0af75b3123d29791aaaa119
Mimart <mimart.yasmina@gmail.com>
2019-05-09 16:41:39 +0200

    entrepreneuriat

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+//partie yasmina
 
  M.AutoInit();
+
+
+//partie tania
+ function changeClass(){
+   if ( document.getElementById("menu_hamburger").classList.contains('hidden') ) {
+     document.getElementById("menu_hamburger").classList.remove('hidden');
+     document.getElementById("menu_hamburger").classList.add('showed');
+   }
+
+   else {
+     document.getElementById("menu_hamburger").classList.remove('showed');
+     document.getElementById("menu_hamburger").classList.add('hidden');
+   }
+ }
+
+ window.onload = function(){
+       document.getElementById("bouton_hamburger").addEventListener( 'click', changeClass);
+   }
