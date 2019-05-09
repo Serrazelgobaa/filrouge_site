eb7b07282c7f8b7fa1422ece036dbde009d1c3a6
Myriam <myriamb2010@yahoo.fr>
2019-05-09 15:58:46 +0200

    ajout page metiers.html et header tania

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function changeClass(){
+if ( document.getElementById("menu_hamburger").classList.contains('hidden') ) {
+document.getElementById("menu_hamburger").classList.remove('hidden');
+document.getElementById("menu_hamburger").classList.add('showed');
+}
+
+else {
+document.getElementById("menu_hamburger").classList.remove('showed');
+document.getElementById("menu_hamburger").classList.add('hidden');
+}
+}
+
+window.onload = function(){
+  document.getElementById("bouton_hamburger").addEventListener( 'click', changeClass);
+}
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.tooltipped');
+   var instances = M.Tooltip.init(elems, 0, 'top');
+ });
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, 0, 'top');
+  });
+
   document.addEventListener('DOMContentLoaded', function() {
       var elems = document.querySelectorAll('.modal');
       var instances = M.Modal.init(elems, 0.5);
